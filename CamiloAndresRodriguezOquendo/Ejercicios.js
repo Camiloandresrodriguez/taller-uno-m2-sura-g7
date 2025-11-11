e2347261130300a7061010d306f7a88b617eac54
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-11 18:46:24 -0500

    Se sealiza ejercicio #05(fila) del taller 1 metodos mutables

--- CamiloAndresRodriguezOquendo/Ejercicios.js
+++ CamiloAndresRodriguezOquendo/Ejercicios.js
@@ -31,4 +31,13 @@ colaImpresion.shift();
 
 console.log(colaImpresion);
 
+//Ejercicio 5
+let fila = ["Ana", "Luis", "Carlos", "Maria"];
+fila.splice(1,1);
+fila.splice(1,0,"Jorge");
+
+console.log(fila);
+
+
+
 
