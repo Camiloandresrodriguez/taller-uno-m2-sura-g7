87c543cb3542ae766b3eb085365034257ab233d0
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-11 21:51:59 -0500

    Se realiza el ejercicio #17(Manejo de Feed) del taller 1 metodos mutables

--- CamiloAndresRodriguezOquendo/Ejercicios.js
+++ CamiloAndresRodriguezOquendo/Ejercicios.js
@@ -113,6 +113,12 @@ parking.splice(0,2,"MotoA", "MotoB");
 
 console.log(parking);
 
+//Ejercicio 17
+let feed = ["Foto de Ana", "Video de Juan", "Noticia de Empresa"];
+feed.unshift("Alerta de Sistema");
+feed.pop("Noticia de Empresa");  
+
+console.log(feed);
 
 
 
