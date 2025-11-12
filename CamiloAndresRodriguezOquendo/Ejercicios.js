9e1c2082dbf519eab55378d9cdb87da02db3624a
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-11 22:02:20 -0500

    Se realiza el ejercicio #18(Lista de Invitados VIP) del taller 1 metodos mutables

--- CamiloAndresRodriguezOquendo/Ejercicios.js
+++ CamiloAndresRodriguezOquendo/Ejercicios.js
@@ -120,6 +120,14 @@ feed.pop("Noticia de Empresa");
 
 console.log(feed);
 
+//Ejercicio 18
+let invitados = ["Carlos", "Beatriz", "David", "Ana"];
+invitados.sort();
+invitados.splice(1,1);
+
+console.log(invitados);
+
+
 
 
 
