a4785014404d7b006dfce02f2358487cf031b105
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-11 21:13:07 -0500

    Se realiza el ejercicio #12(Control de versiones) del taller 1 metodos mutables

--- CamiloAndresRodriguezOquendo/Ejercicios.js
+++ CamiloAndresRodriguezOquendo/Ejercicios.js
@@ -77,7 +77,13 @@ carrito.splice(1,1);
 
 console.log(carrito);
 
+//Ejercicio 12
+let commits = ["v1.0", "v1.1"];
+commits.push("v1.2");
+commits.pop("v1.2");
+commits.push("v1.2-corregido");
 
+console.log(commits);
 
 
 
